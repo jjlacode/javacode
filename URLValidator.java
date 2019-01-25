2ddf0e5bdcd59fac17ded81ce5ffae8e9b382c9d
jjlacode <44982138+jjlacode@users.noreply.github.com>
2019-01-25 20:13:54 +0100

    Validador de urls

---
+++ URLValidator.java
@@ -0,0 +1,24 @@
+// Programa Java para verificar si una URL es válida usando Apache
+// common validator.
+import org.apache.commons.validator.routines.UrlValidator;
+
+class URLValidator {
+    public static boolean urlValidator(String url)
+    {
+        /* Obteniendo UrlValidator */
+        UrlValidator defaultValidator = new UrlValidator();
+        return defaultValidator.isValid(url);
+    }
+
+    public static void main(String[] args)
+    {
+       System.out.println("Introducir web a comprobar");
+       String url = System.console().readLine();
+
+        /* validar una url */
+        if (urlValidator(url))
+            System.out.print("La url dada " + url + " es válida");
+        else
+            System.out.print("La url dada " + url + " no es válida");
+    }
+}
